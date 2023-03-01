a72c820d3034c063422c5799efba6886ae15e204
property09 <122277298+property09@users.noreply.github.com>
2023-03-01 15:48:46 +0300

    myyyyyyyyyyyyyyyyy

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var area = document.getElementById('area');
-var square = document.getElementsByClassName('square')
-var win = document.getElementsByClassName("win")
+var square = document.getElementsByClassName('square');
+var win = document.getElementsByClassName('win');
 
-var player = "X";
-var winIndex = [
+var player = 'X';
+var winInd = [
     [1,2,3],
     [4,5,6],
     [7,8,9],
@@ -16,67 +16,64 @@ var winIndex = [
     [3,5,7]
 ]
 
-for(var i = 1; i <= 9; i++) {
+for (var i = 1; i <= 9; i++) {
     area.innerHTML += "<div class='square' pos=" + i + "></div>"
 }
 
-for (var i = 0; i < square.length; i++) {
+//без false - ошибка
+for(var i = 0; i < 9; i++) {
     square[i].addEventListener('click', squareClick, false)
 }
 
-function squareClick () {
-    var data = [];
+function squareClick() {
+    var data = []
 
     if(!this.innerHTML) {
-        this.innerHTML = player;    
+        this.innerHTML = player
     }
     else {
-        document.getElementById("now").innerHTML = "Клетка занята!:C"
+        document.getElementById('now').innerHTML = "Клетка занята!:C"
         return;
     }
 
-    for(var i in square){
-        if(square[i].innerHTML == player){
+    for(var i in square) {
+        if(square[i].innerHTML == player) {
             data.push(parseInt(square[i].getAttribute('pos')))
         }
     }
-    
-    
+
     console.log(data)
 
-    if (chechWin(data)) {
-        console.log(player)
+    if(checkWin(data)) {
 
-        document.getElementById("who").innerHTML = player
-        document.querySelector(".win").classList.replace("win", "win1")
-
+        document.getElementById('who').innerHTML = player
+        document.querySelector('.win').classList.replace('win', 'win1')
     }
-    
-    if (data.length == 5) {
-        document.getElementById("winner").innerHTML = "Ничья!"
-        document.querySelector(".who").classList.replace("who", "who1")
-        document.querySelector(".win").classList.replace("win", "win1")
 
+    if(data.length >= 5) {
+        document.getElementById('winner').innerHTML = "Ничья!"
+        document.querySelector('.who').classList.replace('who', 'who1')
+        document.querySelector('.win').classList.replace('win', 'win1')
     }
-    player = player == 'X' ? '0' : 'X';
-    document.getElementById("now").innerHTML = "Сейчас ходит: " + player
 
+    player = player == 'X' ? '0' : 'X'
+    document.getElementById('now').innerHTML = "Сейчас ходит: " + player
 }
 
-function chechWin(data) {
-    for(var i in winIndex) {
-        var win = true;
+function checkWin(data) {
+    for(var i in winInd) {
+        win = true
 
-        for(var j in winIndex[i]) {
-            var id = winIndex[i][j];
-            var ind = data.indexOf(id);
+        for (var j in winInd[i]) {
+            var value = winInd[i][j]
+            var index = data.indexOf(value)
 
-            if(ind == -1) {
+            if (index == -1)  {
                 win = false
             }
         }
 
-        if(win) return true;
+        if (win) return true;
     }
     return false;
 }
